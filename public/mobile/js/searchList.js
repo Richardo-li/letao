907a550ee17df7362061282b5849bbb375e0ec1c
1755621107@qq.com <huan>
2017-11-24 09:55:45 +0800

    完成搜索中心

--- public/mobile/js/searchList.js
+++ public/mobile/js/searchList.js
@@ -1,5 +1,18 @@
 $(function() {
 
+    // 全局参数查询对象
+    var queryObj = {
+        proName: "",
+        brandId: "",
+        price: "",
+        num: "",
+        page: 1,
+        pageSize: 6
+    };
+    queryObj.proName = getURLParams("key");
+
+    // 总条数
+    var Count = 1;
 
 
     mui.init({
@@ -12,44 +25,106 @@ $(function() {
                 contentover: "释放立即刷新", //可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
                 contentrefresh: "正在刷新...", //可选，正在刷新状态时，下拉刷新控件上显示的标题内容
                 callback: function() {
-                    // setTimeout(() => {
-                    //     var queryObj = {
-                    //         proName: "",
-                    //         brandId: "",
-                    //         price: "",
-                    //         num: "",
-                    //         page: 1,
-                    //         pageSize: 6
-                    //     };
-                    //     $.ajax({
-                    //         type: 'get',
-                    //         url: "/product/queryProduct",
-                    //         data: queryObj,
-                    //         success: function(result) {
-                    //             // callback && callback(result);
-                    //             // html(html)
-                    //             // append(html) 
-                    //             console.log(result)
-                    //         }
-                    //     })
-
-                    // }, 1000);
-
+                    setTimeout(function() {
+                        queryObj.page = 1;
+                        queryProduct(function(result) {
+                            Count = result.count;
+                            var html = template("mytemp", result);
+                            $(".lt_product_list").html(html);
+                            // 结束下拉刷新
+                            mui('#refreshContainer').pullRefresh().endPulldownToRefresh(false);
+                            // 重置上拉控件的用户提示
+                            mui('#refreshContainer').pullRefresh().refresh(true);
+                        });
+                    }, 1000)
                 }
             },
             up: {
-                height: 50, //可选.默认50.触发上拉加载拖动距离
+                height: 100, //可选.默认50.触发上拉加载拖动距离
                 auto: true, //可选,默认false.自动上拉加载一次
                 contentrefresh: "正在加载...", //可选，正在加载状态时，上拉加载控件上显示的标题内容
                 contentnomore: '没有更多数据了', //可选，请求完毕若没有更多数据时显示的提醒内容；
                 callback: function() {
-                    mui('#refreshContainer').pullRefresh().endPullupToRefresh(false);
-
-                    mui('#refreshContainer').pullRefresh().endPullupToRefresh(true);
+                    var totalPage = Math.ceil(Count / queryObj.pageSize);
+                    setTimeout(function() {
+                        // 判断
+                        if (queryObj.page < totalPage) {
+                            // 继续请求数据
+                            queryObj.page++;
+                            queryProduct(function(result) {
+                                var html = template("mytemp", result)
+                                $(".lt_product_list").append(html);
+                                // 有数据 传入 false  则无其他显示 
+                                mui('#refreshContainer').pullRefresh().endPullupToRefresh(false);
+                            })
+                        } else {
+                            // 没有数据就传入 true  给出用户提示 没有数据了
+                            mui('#refreshContainer').pullRefresh().endPullupToRefresh(true);
+                        }
+                    }, 1000);
                 }
             }
         }
     });
+
+
+
+    /* 
+    1 初始化数据
+      a 获取url的参数 ?key=1 -> 1 
+      b 拼接url
+      c 渲染数据 
+    2 上拉加载下一页
+      一共有多少页 
+      总条数 和我们自己定的页容量 有关系 
+     */
+
+    // 发送请求
+    function queryProduct(callback) {
+        $.ajax({
+            url: "/product/queryProduct",
+            data: queryObj,
+            success: function(result) {
+                callback && callback(result);
+                // html(html)
+                // append(html) 
+            }
+        });
+    }
+
+
+
+    // 获取url上的参数的 
+    function getURLParams(name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            return unescape(r[2]);
+        }
+        return null;
+    }
+
+    // 点击搜索
+    $(".search_btn").on("tap", function() {
+        var val = $(".search_text").val();
+
+        if (!$.trim(val)) {
+            // 用户提示 请输入关键字
+            mui.toast("请输入关键字");
+        } else {
+            queryObj.proName = val;
+            // queryProduct();
+            //  console.log( mui('#refreshContainer').pullRefresh().pullLoading);
+
+            // 手动触发下拉 
+            mui("#refreshContainer").pullRefresh().pulldownLoading();
+        }
+
+
+
+
+
+    })
 
 
 
@@ -64,6 +139,30 @@ $(function() {
         //  mui-icon-arrowdown 改变箭头
         $(this).find(".mui-icon").toggleClass("mui-icon-arrowdown mui-icon-arrowup");
 
+
+        var sort = -1;
+        // 判断 如果 span 上 up 升序    down 降序
+        if ($(this).find(".mui-icon").hasClass("mui-icon-arrowup")) {
+            // 要 升序
+            sort = 1;
+        } else {
+            // 降序
+            sort = 2;
+        }
+
+        // 获取要排序的关键字
+        // console.log($(this).data("sortname"));
+        if ($(this).data("sortname") == "price") {
+            queryObj.price = sort;
+            queryObj.num = "";
+        }
+        if ($(this).data("sortname") == "num") {
+            queryObj.num = sort;
+            queryObj.price = "";
+        }
+
+        // 手动触发下拉 
+        mui("#refreshContainer").pullRefresh().pulldownLoading();
 
 
     })
